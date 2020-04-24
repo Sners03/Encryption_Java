2136ae4ce31a5393efafc3c75b1b7e908306cc01
Sners03 <reichelarslukas@gmail.com>
2020-04-25 00:11:59 +0200

    finished verschluesseln method

--- Encryption_Java/src/ceasar_Code.java
+++ Encryption_Java/src/ceasar_Code.java
@@ -1,17 +1,40 @@
 import java.util.Scanner;
 
 public class ceasar_Code {
-	public static String verschluesseln(String input) {
-		return input;
+	public static String verschluesseln(String text, Integer key) {
+		final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		String encoded = "";
+
+		char current;
+
+		Integer pos; // position 
+		Integer new_key_pos;
+		
+
+		for (int i = 0; i < text.length(); i++){
+			current = text.charAt(i);
+			pos = alphabet.indexOf(current);
+			if(pos == -1){
+				encoded += current;
+				continue;
+			}
+
+			new_key_pos = (pos + key) % 26;
+
+			encoded += (alphabet.charAt(new_key_pos));
+		}
+		return encoded;
 	}
-	public static void main(String[]args) {
-		System.out.println("Hello World");
+
+	public static void main(final String[]args) {
+		final Scanner Scanner = new Scanner(System.in);
 		System.out.print("Gib einen unverschlüsselten String ein: ");
+		String text = (Scanner.nextLine()).toLowerCase();
 
-		Scanner Scanner = new Scanner(System.in);
-		String input = Scanner.nextLine();
+		System.out.print("Gib nun einen Schlüssel an: ");
+		Integer key = Scanner.nextInt();
 
-		String v = verschluesseln(input);
+		String v = verschluesseln(text, key);
 		System.out.println(v);
 	}
 }

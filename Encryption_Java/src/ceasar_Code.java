5f20f35866f5c38e2eb17657542316bd18c43696
Sners03 <reichelarslukas@gmail.com>
2020-04-24 23:32:55 +0200

    added a Scanner objekt

--- Encryption_Java/src/ceasar_Code.java
+++ Encryption_Java/src/ceasar_Code.java
@@ -1,6 +1,17 @@
-public class ceaser {
+import java.util.Scanner;
+
+public class ceasar_Code {
+	public static String verschluesseln(String input) {
+		return input;
+	}
 	public static void main(String[]args) {
 		System.out.println("Hello World");
-		System.out.println("loool");
+		System.out.print("Gib einen unverschlüsselten String ein: ");
+
+		Scanner Scanner = new Scanner(System.in);
+		String input = Scanner.nextLine();
+
+		String v = verschluesseln(input);
+		System.out.println(v);
 	}
 }
